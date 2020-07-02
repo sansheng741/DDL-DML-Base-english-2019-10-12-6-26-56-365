0874280709b5803e1a9972651c3dfa46618c8c1a
sansheng <807156237@qq.com>
2020-07-02 15:47:04 +0800

    数据库练习 Signed-off-by: sansheng <807156237@qq.com>

--- ddl.sql
+++ ddl.sql
@@ -1,31 +1,32 @@
 -- **Database level:**
 -- Display all the database
-
+show databases;
 -- Enter a certain database
-
+use student_examination_sys;
 -- Create a database
-
+create database student;
 -- Create the database of the designated character set
-
+create database student character set  utf8;
 -- Display the creation information fo the database
-
+show create database student;
 -- Revise the codes of the database
-
+alter database student character set gb2312;
 -- Delete a database
-
+drop database student;
 -- **Table level**
 -- Revise table name
-
+alter table user rename db_user;
 -- Revise the field's data type
-
+alter table db_user modify name varchar(16);
 -- Revise field name
-
+alter table db_user change name user_name varchar(16);
 -- Add field
-
+alter table db_user add tel varchar(11);
 -- Delete field
-
+alter table db_user drop tel;
 -- Revise the table's storage engine
-
+alter table db_user engine=MyISAM;
 -- Delete the table's foreign key restriant
-
+alter table db_user drop foreign key fk_name;   
 -- Delete a table
+drop table db_user;
--- dml.sql
+++ dml.sql
@@ -1,7 +1,8 @@
 -- Insert record
-
+insert into db_user(id,user_name,age,sex,addr) values(null,'zhangsan',18,'male','zhuhai');
 -- Revise record
-
---  Delete record
-
+update db_user set age = 20 where id = 1;
+-- Delete record
+delete db_user where id = 1;
 -- Search record
+select * from db_user;
